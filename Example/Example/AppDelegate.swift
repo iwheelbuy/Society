bdef57dd6bda456ece25fc61fe71767c6c4c8cd7
Mikhail Vasilev <iwheelbuy@gmail.com>
2018-03-25 21:27:31 +0700

    0.2.0

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -1,7 +1,6 @@
 import SafariServices
 import SocialNetwork
 import UIKit
-
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -13,25 +12,32 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         SocialNetwork.delegate = self
-        SocialNetwork.Facebook.informationProviderSimplified = self
-        SocialNetwork.Google.informationProviderSimplified = self
-        SocialNetwork.Odnoklassniki.informationProviderSimplified = self
-        SocialNetwork.Vkontakte.informationProviderSimplified = self
+        SocialNetwork.Facebook.dataSource = self
+        SocialNetwork.Google.dataSource = self
+        SocialNetwork.Odnoklassniki.dataSource = self
+        SocialNetwork.Vkontakte.dataSource = self
         window?.rootViewController = UIViewController()
         window?.makeKeyAndVisible()
         defer {
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
-//                let controller = SFSafariViewController(url: SocialNetwork.Facebook.url)
+                switch SocialNetwork.Facebook.nativeExists {
+                case true:
+                    UIApplication.shared.openURL(SocialNetwork.Facebook.nativeUrl)
+                case false:
+                    let controller = SFSafariViewController(url: SocialNetwork.Facebook.oauthUrl)
+                    self?.window?.rootViewController?.present(controller, animated: true)
+                }
 //                let controller = SFSafariViewController(url: SocialNetwork.Google.url)
 //                let controller = SFSafariViewController(url: SocialNetwork.Odnoklassniki.url)
-                let controller = SFSafariViewController(url: SocialNetwork.Vkontakte.url)
-                self?.window?.rootViewController?.present(controller, animated: true)
+//                let controller = SFSafariViewController(url: SocialNetwork.Vkontakte.url)
+                
             }
         }
         return true
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        print(url)
         if SocialNetwork.didProceed(url: url) {
             return true
         }
@@ -59,30 +65,30 @@ extension AppDelegate: SocialNetworkDelegate {
     }
 }
 
-extension AppDelegate: SocialNetworkFacebookInformationProviderSimplified {
+extension AppDelegate: SocialNetworkFacebookDataSource {
     
-    func socialNetworkFacebookApplicationIdentifier() -> String {
+    func socialNetworkFacebookClientIdentifier() -> String {
         return "570084943360654"
     }
 }
 
-extension AppDelegate: SocialNetworkGoogleInformationProviderSimplified {
+extension AppDelegate: SocialNetworkGoogleDataSource {
     
-    func socialNetworkGoogleApplicationIdentifier() -> String {
+    func socialNetworkGoogleClientIdentifier() -> String {
         return "683698461214-h7n4hki1pagc5d7fvveq4fbb3baolt72"
     }
 }
 
-extension AppDelegate: SocialNetworkOdnoklassnikiInformationProviderSimplified {
+extension AppDelegate: SocialNetworkOdnoklassnikiDataSource {
     
-    func socialNetworkOdnoklassnikiApplicationIdentifier() -> String {
+    func socialNetworkOdnoklassnikiClientIdentifier() -> String {
         return "1264616960"
     }
 }
 
-extension AppDelegate: SocialNetworkVkontakteInformationProviderSimplified {
+extension AppDelegate: SocialNetworkVkontakteDataSource {
     
-    func socialNetworkVkontakteApplicationIdentifier() -> String {
+    func socialNetworkVkontakteClientIdentifier() -> String {
         return "6357688"
     }
 }
--- Sources/SocialNetwork.swift
+++ Sources/SocialNetwork.swift
@@ -1,11 +1,22 @@
 import Foundation
 
-let urlQueryAllowedSet = CharacterSet.urlQueryAllowed
+private let urlRedirectSimplified = "https://iwheelbuy.github.io/SocialNetwork/simplified.html"
+private let urlQueryAllowedSet = CharacterSet.urlQueryAllowed
 
 extension String {
     
     var urlQueryConverted: String? {
         return addingPercentEncoding(withAllowedCharacters: urlQueryAllowedSet)
+    }
+    
+    var queryItems: [String: String] {
+        guard let items = URLComponents(string: self)?.queryItems else {
+            return [:]
+        }
+        return items
+            .reduce(into: [String:String]()) { (dictionary, item) in
+                dictionary[item.name] = item.value
+        }
     }
 }
 
@@ -29,32 +40,32 @@ public protocol SocialNetworkDelegate: class {
     func socialNetwork(socialNetwork: SocialNetwork, didCompleteWithParameters parameters: [String: String])
 }
 
-// MARK: - SocialNetworkFacebookInformationProviderSimplified
+// MARK: - SocialNetworkFacebookDataSource
 
-public protocol SocialNetworkFacebookInformationProviderSimplified: class {
+public protocol SocialNetworkFacebookDataSource: class {
     
-    func socialNetworkFacebookApplicationIdentifier() -> String
+    func socialNetworkFacebookClientIdentifier() -> String
 }
 
-// MARK: - SocialNetworkGoogleInformationProviderSimplified
+// MARK: - SocialNetworkGoogleDataSource
 
-public protocol SocialNetworkGoogleInformationProviderSimplified: class {
+public protocol SocialNetworkGoogleDataSource: class {
     
-    func socialNetworkGoogleApplicationIdentifier() -> String
+    func socialNetworkGoogleClientIdentifier() -> String
 }
 
-// MARK: - SocialNetworkOdnoklassnikiInformationProviderSimplified
+// MARK: - SocialNetworkOdnoklassnikiDataSource
 
-public protocol SocialNetworkOdnoklassnikiInformationProviderSimplified: class {
+public protocol SocialNetworkOdnoklassnikiDataSource: class {
     
-    func socialNetworkOdnoklassnikiApplicationIdentifier() -> String
+    func socialNetworkOdnoklassnikiClientIdentifier() -> String
 }
 
-// MARK: - SocialNetworkVkontakteInformationProviderSimplified
+// MARK: - SocialNetworkVkontakteDataSource
 
-public protocol SocialNetworkVkontakteInformationProviderSimplified: class {
+public protocol SocialNetworkVkontakteDataSource: class {
     
-    func socialNetworkVkontakteApplicationIdentifier() -> String
+    func socialNetworkVkontakteClientIdentifier() -> String
 }
 
 // MARK: -
@@ -71,14 +82,28 @@ public enum SocialNetwork: String {
     /// Vkontakte
     case vkontakte = "vkontakte"
     ///
+    static func getUrlFrom(path: String, parameters: [String: String]) -> URL {
+        let string = path + "?" + parameters.map({ "\($0.key)=\($0.value)" }).joined(separator: "&")
+        guard let url = URL(string: string.urlQueryConverted ?? string) else {
+            fatalError("Failed to create URL from string: \"\(string)\"")
+        }
+        return url
+    }
+}
+
+public extension SocialNetwork {
+    ///
     public static func didProceed(url: URL) -> Bool {
-        if didProceedSimplified(url: url) {
+        if didProceedOauthSimplified(url: url) {
+            return true
+        }
+        if didProceedNativeSimplified(url: url) {
             return true
         }
         return false
     }
     ///
-    static func didProceedSimplified(url: URL) -> Bool {
+    static func didProceedOauthSimplified(url: URL) -> Bool {
         guard url.scheme?.lowercased() == "socialnetwork" else {
             return false
         }
@@ -99,98 +124,154 @@ public enum SocialNetwork: String {
         return true
     }
     ///
-    static func getSimplifiedRedirectUrl() -> String {
-        return "https://iwheelbuy.github.io/SocialNetwork/simplified.html"
+    static func didProceedNativeSimplified(url: URL) -> Bool {
+        if didProceedNativeFacebookSimplified(url: url) {
+            return true
+        }
+        return false
     }
     ///
-    static func getSimplifiedUrl(path: String, parameters: [String: String]) -> URL {
-        let string = path + "?" + parameters.map({ "\($0.key)=\($0.value)" }).joined(separator: "&")
-        guard let url = URL(string: string.urlQueryConverted ?? string) else {
-            fatalError("Failed to create URL from string: \"\(string)\"")
+    static func didProceedNativeFacebookSimplified(url: URL) -> Bool {
+        guard let provider = SocialNetwork.Facebook.dataSource else {
+            return false
         }
-        return url
+        let scheme = "fb" + provider.socialNetworkFacebookClientIdentifier()
+        guard scheme == url.scheme else {
+            return false
+        }
+        defer {
+            let parameters = url.absoluteString.replacingOccurrences(of: scheme + "://authorize#", with: scheme + "://authorize?").queryItems
+            SocialNetwork.delegate?.socialNetwork(socialNetwork: SocialNetwork.facebook, didCompleteWithParameters: parameters)
+        }
+        return true
     }
+}
+
+// MARK: - Facebook
+
+public extension SocialNetwork {
     /// Facebook
     public final class Facebook {
         ///
-        public static weak var informationProviderSimplified: SocialNetworkFacebookInformationProviderSimplified?
+        public static weak var dataSource: SocialNetworkFacebookDataSource?
         ///
-        public static var url: URL {
-            if let informationProviderSimplified = informationProviderSimplified {
+        public static var oauthUrl: URL {
+            if let dataSource = dataSource {
                 let path = "https://www.facebook.com/v2.12/dialog/oauth"
                 let parameters = [
-                    "client_id": informationProviderSimplified.socialNetworkFacebookApplicationIdentifier(),
-                    "redirect_uri": SocialNetwork.getSimplifiedRedirectUrl(),
+                    "client_id": dataSource.socialNetworkFacebookClientIdentifier(),
+                    "redirect_uri": urlRedirectSimplified,
                     "state": "facebook",
                     "response_type": "token",
                     "scope": "email"
                 ]
-                return SocialNetwork.getSimplifiedUrl(path: path, parameters: parameters)
+                return SocialNetwork.getUrlFrom(path: path, parameters: parameters)
             }
-            fatalError("SocialNetworkFacebookInformationProviderSimplified doesn't exist")
+            fatalError("SocialNetworkFacebookDataSource doesn't exist")
+        }
+        public static var nativeExists: Bool {
+            return ["fb://", "fbapi://", "fbauth://", "fbauth2://"]
+                .flatMap({ Foundation.URL(string: $0) })
+                .map({ UIApplication.shared.canOpenURL($0) })
+                .filter({ $0 == false })
+                .count == 0
+        }
+        ///
+        public static var nativeUrl: URL {
+            if let dataSource = dataSource {
+                let path = "fbauth://authorize"
+                let parameters = [
+                    "client_id": dataSource.socialNetworkFacebookClientIdentifier(),
+                    "sdk": "ios",
+                    "return_scopes": "true",
+                    "redirect_uri": "fbconnect%3A%2F%2Fsuccess",
+                    "scope": "email",
+                    "display": "touch",
+                    "response_type": "token",
+                    "legacy_override": "v2.6",
+                    "sdk_version": "4.7"
+                ]
+                return SocialNetwork.getUrlFrom(path: path, parameters: parameters)
+            }
+            fatalError("SocialNetworkFacebookDataSource doesn't exist")
         }
     }
+}
+
+// MARK: - Google
+
+public extension SocialNetwork {
     /// Google
     public final class Google {
         ///
-        public static weak var informationProviderSimplified: SocialNetworkGoogleInformationProviderSimplified?
+        public static weak var dataSource: SocialNetworkGoogleDataSource?
         ///
         public static var url: URL {
-            if let informationProviderSimplified = informationProviderSimplified {
+            if let dataSource = dataSource {
                 let path = "https://accounts.google.com/o/oauth2/v2/auth"
                 let parameters = [
-                    "client_id": informationProviderSimplified.socialNetworkGoogleApplicationIdentifier() + ".apps.googleusercontent.com",
-                    "redirect_uri": SocialNetwork.getSimplifiedRedirectUrl(),
+                    "client_id": dataSource.socialNetworkGoogleClientIdentifier() + ".apps.googleusercontent.com",
+                    "redirect_uri": urlRedirectSimplified,
                     "state": "google",
                     "response_type": "token",
                     "scope": "email"
                 ]
-                return SocialNetwork.getSimplifiedUrl(path: path, parameters: parameters)
+                return SocialNetwork.getUrlFrom(path: path, parameters: parameters)
             }
-            fatalError("SocialNetworkGoogleInformationProviderSimplified doesn't exist")
+            fatalError("SocialNetworkGoogleDataSource doesn't exist")
         }
     }
+}
+
+// MARK: - Odnoklassniki
+
+public extension SocialNetwork {
     /// Odnoklassniki
     public final class Odnoklassniki {
         ///
-        public static weak var informationProviderSimplified: SocialNetworkOdnoklassnikiInformationProviderSimplified?
+        public static weak var dataSource: SocialNetworkOdnoklassnikiDataSource?
         ///
         public static var url: URL {
-            if let informationProviderSimplified = informationProviderSimplified {
+            if let dataSource = dataSource {
                 let path = "https://connect.ok.ru/oauth/authorize"
                 let parameters = [
-                    "client_id": informationProviderSimplified.socialNetworkOdnoklassnikiApplicationIdentifier(),
-                    "redirect_uri": SocialNetwork.getSimplifiedRedirectUrl(),
+                    "client_id": dataSource.socialNetworkOdnoklassnikiClientIdentifier(),
+                    "redirect_uri": urlRedirectSimplified,
                     "state": "odnoklassniki",
                     "response_type": "token",
                     "scope": "GET_EMAIL",
                     "layout": "m"
                 ]
-                return SocialNetwork.getSimplifiedUrl(path: path, parameters: parameters)
+                return SocialNetwork.getUrlFrom(path: path, parameters: parameters)
             }
-            fatalError("SocialNetworkOdnoklassnikiInformationProviderSimplified doesn't exist")
+            fatalError("SocialNetworkOdnoklassnikiDataSource doesn't exist")
         }
     }
+}
+
+// MARK: - Vkontakte
+
+public extension SocialNetwork {
     /// Vkontakte
     public final class Vkontakte {
         ///
-        public static weak var informationProviderSimplified: SocialNetworkVkontakteInformationProviderSimplified?
+        public static weak var dataSource: SocialNetworkVkontakteDataSource?
         ///
         public static var url: URL {
-            if let informationProviderSimplified = informationProviderSimplified {
+            if let dataSource = dataSource {
                 let path = "https://oauth.vk.com/authorize"
                 let parameters = [
-                    "client_id": informationProviderSimplified.socialNetworkVkontakteApplicationIdentifier(),
-                    "redirect_uri": SocialNetwork.getSimplifiedRedirectUrl(),
+                    "client_id": dataSource.socialNetworkVkontakteClientIdentifier(),
+                    "redirect_uri": urlRedirectSimplified,
                     "state": "vkontakte",
                     "response_type": "token",
                     "scope": "email",
                     "revoke": "1",
                     "v": "5.73"
                 ]
-                return SocialNetwork.getSimplifiedUrl(path: path, parameters: parameters)
+                return SocialNetwork.getUrlFrom(path: path, parameters: parameters)
             }
-            fatalError("SocialNetworkVkontakteInformationProviderSimplified doesn't exist")
+            fatalError("SocialNetworkVkontakteDataSource doesn't exist")
         }
     }
 }
